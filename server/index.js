127214501f00d2e8bb055d8d33c1047ad44c881c
Patryk Okpara <patrykokpara@hotmail.com>
2022-06-16 00:12:39 +0100

    Connected to the database

--- server/index.js
+++ server/index.js
@@ -7,3 +7,13 @@ const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://lorn888:limpiki1@cluster0.iwmsk.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`Server running on pott ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
